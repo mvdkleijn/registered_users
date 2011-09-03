fd3b43ddedbef2880899033f8b6286d98c0394e2
Martijn <martijn.niji@gmail.com>
2011-09-03 20:20:00 +0200

    Added uninstall functionality

---
+++ uninstall.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the "Registered Users" plugin for Wolf CMS.
+ * Licensed under an MIT style license. For full details see license.txt.
+ *
+ * @author Martijn van der Kleijn <martijn.niji@gmail.com>
+ * @copyright Martijn van der Kleijn, 2011
+ * 
+ */
+
+/* Prevent direct access. */
+if (!defined('IN_CMS')) { exit(); }
+
+$sql = 'DROP TABLE IF EXISTS `'.TABLE_PREFIX.'registered_users_temp`';
+$pdo = Record::getConnection();
+$pdo->exec($sql);
+
+$sql = 'DROP TABLE IF EXISTS `'.TABLE_PREFIX.'permission_page`';
+$pdo = Record::getConnection();
+$pdo->exec($sql);
+
+$sql = 'DROP TABLE IF EXISTS `'.TABLE_PREFIX.'registered_users_settings`';
+$pdo = Record::getConnection();
+$pdo->exec($sql);
+
+?>
